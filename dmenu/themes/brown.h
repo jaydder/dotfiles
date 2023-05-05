e52f62a573be2333b880cd551249a474e400f275
jaydder <jaydderfavoravel@gmail.com>
2023-05-05 18:22:17 -0300

    New config 18/05/23

---
+++ dmenu/themes/brown.h
@@ -0,0 +1,6 @@
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#ffffff", "#16161E" },
+	[SchemeSel] = { "#ffffff", "#7e4438" },
+	[SchemeOut] = { "#000000", "#66887c" },
+};
---
+++ dmenu/themes/normal.h
@@ -0,0 +1,6 @@
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#ffffff", "#0f0f15" },
+	[SchemeSel] = { "#ffffff", "#36364a" },
+	[SchemeOut] = { "#000000", "#66887c" },
+};
